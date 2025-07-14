f2c70f883aa806c611baaa5bf5fa4702b41b16fe
mshortcodes <mshortcodes@gmail.com>
2025-07-14 13:25:50 -0700

    refactor

--- main.go
+++ main.go
@@ -16,33 +16,48 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("====================")
 
-	var currentLineContents string
-	buffer := make([]byte, 8)
-	for {
-		n, err := f.Read(buffer)
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
-			if errors.Is(err, io.EOF) {
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+
+		var currentLineContents string
+		buffer := make([]byte, 8)
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err)
 				break
 			}
-			fmt.Printf("error: %s\n", err)
-			break
-		}
-		str := string(buffer[:n])
+			str := string(buffer[:n])
 
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
-		currentLineContents += parts[len(parts)-1]
-	}
+	}()
+	return lines
 }
